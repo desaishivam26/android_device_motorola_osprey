e11b9d0f199b2c9dbae814c0a53de3dce9106cdb
desaishivam26 <shivamdesaixda@gmail.com>
2017-02-20 14:30:07 +0530

    slim time
    Change-Id: If09e588f48bb7f8a1be82c900095608d7fce3d06

--- slim.mk
+++ slim.mk
@@ -21,7 +21,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := lineage_osprey
+PRODUCT_NAME := slim_osprey
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
